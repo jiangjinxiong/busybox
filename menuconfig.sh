3621adb79c0359f84b4bece56418482830032933
jiangjinxiong <jiangjinxiong@tongfangcloud.com>
2013-08-25 21:23:21 +0800

    update

---
+++ menuconfig.sh
@@ -0,0 +1,5 @@
+export ARCH=arm
+export CROSS_COMPILE=arm_v5t_le-
+export KBUILD_OUTPUT=obj
+
+make menuconfig 
---
+++ mkbusybox.sh
@@ -0,0 +1,9 @@
+
+export ARCH=arm
+#export CROSS_COMPILE=arm_v5t_le-
+export CROSS_COMPILE=arm-none-linux-gnueabi-
+export KBUILD_OUTPUT=obj
+
+make  
+
+
